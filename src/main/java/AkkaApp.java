c27677781e31022e02f67bb4f043dc1fb985b76c
ramagf <ramagf@yandex.ru>
2021-12-04 13:13:28 +0300

    Scripted auto-commit on change (2021-12-04 13:13:28) by gitwatch.sh

--- src/main/java/AkkaApp.java
+++ src/main/java/AkkaApp.java
@@ -6,7 +6,7 @@ public class AkkaApp {
     public static void main(String[] args) {
         ActorSystem system = ActorSystem.create("test");
         ActorRef storeActor = system.actorOf(Props.create(ResultActor.class, () -> new ResultActor("test")));
-        
+
         storeActor.tell(Props.create(ResultActor.class), ActorRef.noSender());
 
     }
