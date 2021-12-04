633919b6d19370273967d9ee3470155d3a9cdc8c
ramagf <ramagf@yandex.ru>
2021-12-04 12:57:59 +0300

    Scripted auto-commit on change (2021-12-04 12:57:59) by gitwatch.sh

--- src/main/java/ResultActor.java
+++ src/main/java/ResultActor.java
@@ -7,7 +7,7 @@ public class ResultActor extends AbstractActor {
     @Override
     public Receive createReceive() {
         return receiveBuilder()
-                .match(String.class, s -> s.toUpperCase(Locale.ROOT))
+                .match(String.class, s -> getSender()
     }
 
     public Object StoreMessage(String test) {
