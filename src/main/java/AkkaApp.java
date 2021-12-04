4fc52e34f6f28f0de1aa09f9fcc61467d5eb28b6
ramagf <ramagf@yandex.ru>
2021-12-04 12:41:59 +0300

    Scripted auto-commit on change (2021-12-04 12:41:58) by gitwatch.sh

--- src/main/java/AkkaApp.java
+++ src/main/java/AkkaApp.java
@@ -1,8 +1,9 @@
+import akka.actor.ActorRef;
 import akka.actor.Props;
 
 public class AkkaApp {
     public static void main(String[] args) {
         Props props = Props.create(ResultActor.class);
-        
+        ActorRef storeActor = system.actorOf(props);
     }
 }
