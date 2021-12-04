d3665afc295daaeb7ac89034dea0e60288f85682
ramagf <ramagf@yandex.ru>
2021-12-04 12:31:36 +0300

    Scripted auto-commit on change (2021-12-04 12:31:36) by gitwatch.sh

--- src/main/java/ResultActor.java
+++ src/main/java/ResultActor.java
@@ -1,2 +1,8 @@
-public class ResultActor {
+import akka.actor.AbstractActor;
+
+public class ResultActor extends AbstractActor {
+    @Override
+    public Receive createReceive() {
+        return null;
+    }
 }
