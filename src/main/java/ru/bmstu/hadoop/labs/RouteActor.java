590a45125e8c718e0bc89a8c61585fbe80984bd9
ramagf <ramagf@yandex.ru>
2021-12-10 00:26:19 +0300

    Scripted auto-commit on change (2021-12-10 00:26:19) by gitwatch.sh

--- src/main/java/ru/bmstu/hadoop/labs/RouteActor.java
+++ src/main/java/ru/bmstu/hadoop/labs/RouteActor.java
@@ -12,8 +12,8 @@ public class RouteActor extends AbstractActor {
     private static ActorRef executeActor;
 
     public RouteActor() {
-        storeActor = getContext().actorOf(Props.create(StoreActor.class));
-        executeActor = getContext().actorOf(Props.create(ExecuteTestActor.class));
+        storeActor = context().actorOf(Props.create(StoreActor.class));
+        executeActor = context().actorOf(Props.create(ExecuteTestActor.class));
     }
 
     @Override
