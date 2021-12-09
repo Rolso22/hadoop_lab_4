45801acb4629a3ed427c317d58c4d7397f9c3ddb
ramagf <ramagf@yandex.ru>
2021-12-10 00:50:49 +0300

    Scripted auto-commit on change (2021-12-10 00:50:49) by gitwatch.sh

--- src/main/java/ru/bmstu/hadoop/labs/Contracts/PostRequest.java
+++ src/main/java/ru/bmstu/hadoop/labs/Contracts/PostRequest.java
@@ -2,6 +2,8 @@ package ru.bmstu.hadoop.labs.Contracts;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import java.util.ArrayList;
+
 public class PostRequest {
     @JsonProperty("packageId")
     public String packageId;
@@ -9,8 +11,8 @@ public class PostRequest {
     public String jsScript;
     @JsonProperty("functionName")
     public String fnName;
-    @JsonProperty("test")
-    public String fnName;
+    @JsonProperty("tests")
+    public ArrayList<Object> tests;
 
 
 
