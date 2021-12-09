417408bd4cff6e379edeaf81860d526803b4891d
ramagf <ramagf@yandex.ru>
2021-12-10 00:18:30 +0300

    Scripted auto-commit on change (2021-12-10 00:18:30) by gitwatch.sh

--- src/main/java/ru/bmstu/hadoop/labs/StoreActor.java
+++ src/main/java/ru/bmstu/hadoop/labs/StoreActor.java
@@ -1,6 +1,7 @@
 package ru.bmstu.hadoop.labs;
 
 import akka.actor.AbstractActor;
+import akka.actor.ActorRef;
 import akka.japi.pf.ReceiveBuilder;
 import ru.bmstu.hadoop.labs.Contracts.GetRequest;
 
@@ -9,7 +10,7 @@ public class StoreActor extends AbstractActor {
     @Override
     public Receive createReceive() {
         return ReceiveBuilder.create()
-                .match(GetRequest.class, msg -> sender().tell(msg.getPackageId(), );)
+                .match(GetRequest.class, msg -> sender().tell(msg.getPackageId(), ActorRef.noSender()))
                 .build();
     }
 }
