8d8fadc4359a363e4213cac8af26391cdddc23bf
ramagf <ramagf@yandex.ru>
2021-12-10 00:47:15 +0300

    Scripted auto-commit on change (2021-12-10 00:47:14) by gitwatch.sh

--- src/main/java/ru/bmstu/hadoop/labs/Contracts/PostRequest.java
+++ src/main/java/ru/bmstu/hadoop/labs/Contracts/PostRequest.java
@@ -1,6 +1,9 @@
 package ru.bmstu.hadoop.labs.Contracts;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class PostRequest {
+    @JsonProperty()
     private final String packageId;
-    
+
 }
