a83d58484499fc450b86bcaa8a0394335b8738ef
ramagf <ramagf@yandex.ru>
2021-12-09 23:29:13 +0300

    Scripted auto-commit on change (2021-12-09 23:29:13) by gitwatch.sh

--- src/main/java/AkkaApp.java
+++ src/main/java/AkkaApp.java
@@ -42,8 +42,8 @@ public class AkkaApp {
 
     private Route createRoute() {
         return route(
-                get(() -> complete("Received GET"))
-                        .orElse(() -> complete("Received something else"))
+                get(() -> complete("Received GET")),
+                post(() -> complete("Received something else"))
         );
     }
 }
