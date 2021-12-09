2761a45d7667b7edb6d78104d43a582693666adf
ramagf <ramagf@yandex.ru>
2021-12-10 00:55:20 +0300

    Scripted auto-commit on change (2021-12-10 00:55:20) by gitwatch.sh

--- src/main/java/ru/bmstu/hadoop/labs/ExecuteTestActor.java
+++ src/main/java/ru/bmstu/hadoop/labs/ExecuteTestActor.java
@@ -9,7 +9,7 @@ public class ExecuteTestActor extends AbstractActor {
     @Override
     public Receive createReceive() {
         return ReceiveBuilder.create()
-                .match(PostRequest.class, msg -> System.out.println(msg.getFnName()))
+                .match(PostRequest.class, msg -> System.out.println(msg.getFnName() + " " + msg.getTests()))
                 .build();
     }
 }
