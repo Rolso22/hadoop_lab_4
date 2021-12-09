9850db3b772d4c5c893f91a5b03bc0c8d4208b3b
ramagf <ramagf@yandex.ru>
2021-12-09 23:20:10 +0300

    Scripted auto-commit on change (2021-12-09 23:20:10) by gitwatch.sh

--- src/main/java/AkkaApp.java
+++ src/main/java/AkkaApp.java
@@ -1,6 +1,17 @@
+import akka.NotUsed;
+import akka.actor.AbstractActor;
 import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import akka.actor.Props;
+import akka.http.javadsl.ConnectHttp;
+import akka.http.javadsl.Http;
+import akka.http.javadsl.ServerBinding;
+import akka.http.javadsl.model.HttpRequest;
+import akka.http.javadsl.model.HttpResponse;
+import akka.stream.ActorMaterializer;
+import akka.stream.javadsl.Flow;
+
+import java.util.concurrent.CompletionStage;
 
 public class AkkaApp {
     public static void main(String[] args) {
@@ -8,5 +19,19 @@ public class AkkaApp {
         ActorRef actor = system.actorOf(Props.create(StoreActor.class));
         actor.tell("test", ActorRef.noSender());
 
+        final Http http = Http.get(system);
+        final ActorMaterializer materializer = ActorMaterializer.create(system);
+        AkkaApp instance = new MainHttp(system);
+        final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow =
+                instance.createRoute(system).flow(system, materializer);
+        final CompletionStage<ServerBinding> binding = http.bindAndHandle(
+                routeFlow,
+                ConnectHttp.toHost("localhost", 8080),
+                materializer
+        );
+        System.out.println("Server online at http://localhost:8080");
+        System.in.read();
+        binding.thenCompose(ServerBinding::unbind)
+                .thenAccept(unbound -> system.terminate());
     }
 }
