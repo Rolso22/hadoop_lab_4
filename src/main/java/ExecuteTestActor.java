6eaeaa5a54ac641e92559e18f6cf2b2ff1279117
ramagf <ramagf@yandex.ru>
2021-12-09 23:16:47 +0300

    Scripted auto-commit on change (2021-12-09 23:16:47) by gitwatch.sh

--- src/main/java/ExecuteTestActor.java
+++ src/main/java/ExecuteTestActor.java
@@ -1,2 +1,8 @@
-public class ExecuteTestActor {
+import akka.actor.AbstractActor;
+
+public class ExecuteTestActor extends AbstractActor {
+    @Override
+    public Receive createReceive() {
+        return null;
+    }
 }
