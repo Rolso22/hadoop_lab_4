7ff6770a4df808158eea0a65db0d7449d942323c
ramagf <ramagf@yandex.ru>
2021-12-10 00:53:25 +0300

    Scripted auto-commit on change (2021-12-10 00:53:25) by gitwatch.sh

--- src/main/java/ru/bmstu/hadoop/labs/ExecuteTestActor.java
+++ src/main/java/ru/bmstu/hadoop/labs/ExecuteTestActor.java
@@ -2,13 +2,14 @@ package ru.bmstu.hadoop.labs;
 
 import akka.actor.AbstractActor;
 import akka.japi.pf.ReceiveBuilder;
+import ru.bmstu.hadoop.labs.Contracts.PostRequest;
 
 public class ExecuteTestActor extends AbstractActor {
 
     @Override
     public Receive createReceive() {
         return ReceiveBuilder.create()
-                .match(String.class, System.out::println)
+                .match(PostRequest.class, msg -> System.out.println(msg.getFnName()))
                 .build();
     }
 }
