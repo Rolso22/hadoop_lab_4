aa327380aca8ff561160a20bc90d782ea9939195
ramagf <ramagf@yandex.ru>
2021-12-10 15:54:30 +0300

    Scripted auto-commit on change (2021-12-10 15:54:30) by gitwatch.sh

--- src/main/java/ru/bmstu/hadoop/labs/StoreActor.java
+++ src/main/java/ru/bmstu/hadoop/labs/StoreActor.java
@@ -23,6 +23,7 @@ public class StoreActor extends AbstractActor {
         if (!store.containsKey(msg.getPackageId())) {
             store.put(msg.getPackageId(), new HashMap<>());
         }
+        System.out.println(msg.getPackageId() + " " + msg.getName() + " " + msg.getResult());
         store.get(msg.getPackageId()).put(msg.getName(), msg.getResult());
     }
 
