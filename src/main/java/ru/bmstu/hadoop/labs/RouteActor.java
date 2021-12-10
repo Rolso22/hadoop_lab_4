5729bb35b865ba1ce6b535e907ec0dd3e0179fd9
ramagf <ramagf@yandex.ru>
2021-12-10 15:07:26 +0300

    Scripted auto-commit on change (2021-12-10 15:07:26) by gitwatch.sh

--- src/main/java/ru/bmstu/hadoop/labs/RouteActor.java
+++ src/main/java/ru/bmstu/hadoop/labs/RouteActor.java
@@ -51,7 +51,7 @@ public class RouteActor extends AbstractActor {
 
     private void executeTests(PostRequest msg) {
         for (Test test : msg.getTests()) {
-            System.out.println("params: " + test.getFirstParam() + " " + test.getSecondParam());
+            
         }
     }
 
