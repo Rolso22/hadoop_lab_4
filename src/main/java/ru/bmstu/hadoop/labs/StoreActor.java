5f0375fccb22c4154df824143bd7778b29fc7239
ramagf <ramagf@yandex.ru>
2021-12-10 15:47:32 +0300

    Scripted auto-commit on change (2021-12-10 15:47:32) by gitwatch.sh

--- src/main/java/ru/bmstu/hadoop/labs/StoreActor.java
+++ src/main/java/ru/bmstu/hadoop/labs/StoreActor.java
@@ -1,20 +1,25 @@
 package ru.bmstu.hadoop.labs;
 
 import akka.actor.AbstractActor;
-import akka.actor.ActorRef;
 import akka.japi.pf.ReceiveBuilder;
-import ru.bmstu.hadoop.labs.Contracts.GetRequest;
 import ru.bmstu.hadoop.labs.Contracts.Result;
 
+import java.util.HashMap;
+
 public class StoreActor extends AbstractActor {
+    private final HashMap<String, HashMap<String, String>> store = new HashMap<>();
 
     @Override
     public Receive createReceive() {
         return ReceiveBuilder.create()
-                .match(Result.class, msg -> {
-                    
-                })
+                .match(Result.class, this::saveResult)
                 .build();
     }
 
+    private void saveResult(Result msg) {
+        if (!store.containsKey(msg.getPackageId())) {
+            store.put(msg.getPackageId(), new HashMap<>());
+        }
+
+    }
 }
