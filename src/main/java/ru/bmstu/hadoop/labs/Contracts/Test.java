89a5c07758f6741897189527cceaf9d6648184b2
ramagf <ramagf@yandex.ru>
2021-12-10 15:00:13 +0300

    Scripted auto-commit on change (2021-12-10 15:00:13) by gitwatch.sh

--- src/main/java/ru/bmstu/hadoop/labs/Contracts/Test.java
+++ src/main/java/ru/bmstu/hadoop/labs/Contracts/Test.java
@@ -1,7 +1,12 @@
 package ru.bmstu.hadoop.labs.Contracts;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.List;
+
 public class Test {
+    @JsonProperty("")
     public String name;
     public String result;
-    public 
+    public List<Integer> params;
 }
